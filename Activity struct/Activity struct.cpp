e48fed931233ada616fae379807ff29a3234fab1
jaddu17 <shafajaddu@gmail.com>
2024-04-02 09:38:23 +0700

    membuat fungsi int main

--- Activity struct/Activity struct.cpp
+++ Activity struct/Activity struct.cpp
@@ -7,3 +7,15 @@ struct Mahasiswa {
 	string alamat;
 	string umur;
 }
+
+int main() {
+	Mahasiswa mhs;
+	cout << "Nomor Mahasiswa : ";
+	cin >> mhs.nim;
+	cout << "Nama Mahasiswa : ";
+	cin >> mhs.nama;
+	cout << "Alamat Mahasiswa : ";
+	cin >> mhs.alamat;
+	cout << "Umur Mahasiswa : ";
+	cin >> mhs.umur;
+}
