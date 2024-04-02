b1a68b72c0647362a5ac2c4bf0e4e7f51015042c
jaddu17 <shafajaddu@gmail.com>
2024-04-02 10:34:17 +0700

    membuat for

--- Activity struct 4/Activity struct 4.cpp
+++ Activity struct 4/Activity struct 4.cpp
@@ -14,11 +14,12 @@ struct Mahasiswa {
 };
 
 int main() {
-	Mahasiswa mhs;
+	Mahasiswa mhs[3];
+	for (int i = 0; i < 3; i++)
 	cout << "Nomor Mahasiswa : ";
-	getline(cin,mhs.nim);
+	getline(cin, mhs.nim);
 	cout << "Nama Mahasiswa : ";
-	getline(cin,mhs.nama);
+	getline(cin, mhs.nama);
 
 	cout << "Alamat Mahasiswa : " << endl;
 	cout << "\t Nama Desa :";
