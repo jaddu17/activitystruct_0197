8c45f148da61d6fc89db8ece80f5d72c82f44a2e
jaddu17 <shafajaddu@gmail.com>
2024-04-02 10:36:54 +0700

    menampilkan for

--- Activity struct 4/Activity struct 4.cpp
+++ Activity struct 4/Activity struct 4.cpp
@@ -15,25 +15,27 @@ struct Mahasiswa {
 
 int main() {
 	Mahasiswa mhs[3];
-	for (int i = 0; i < 3; i++)
-	cout << "Nomor Mahasiswa : ";
-	getline(cin, mhs.nim);
-	cout << "Nama Mahasiswa : ";
-	getline(cin, mhs.nama);
+	for (int i = 0; i < 3; i++){
+		cout << "Nomor Mahasiswa : ";
+		getline(cin, mhs.nim);
+		cout << "Nama Mahasiswa : ";
+		getline(cin, mhs.nama);
 
-	cout << "Alamat Mahasiswa : " << endl;
-	cout << "\t Nama Desa :";
-	cin >> mhs.alamat.desa;
-	cout << "\t Nama Kota :";
-	cin >> mhs.alamat.kota;
-	cout << "Umur Mahasiswa : ";
-	cin >> mhs.umur;
-
-	cout << endl;
-	cout << "\n NIM : " << mhs.nim;
-	cout << "\n Nama : " << mhs.nama;
-	cout << "\n Alamat : ";
-	cout << "\n \t Desa : " << mhs.alamat.desa;
-	cout << "\n \t Kota : " << mhs.alamat.kota;
-	cout << "\n Umur : " << mhs.umur;
+		cout << "Alamat Mahasiswa : " << endl;
+		cout << "\t Nama Desa :";
+		cin >> mhs.alamat.desa;
+		cout << "\t Nama Kota :";
+		cin >> mhs.alamat.kota;
+		cout << "Umur Mahasiswa : ";
+		cin >> mhs.umur;
+	}
+	for (int i = 0; i < 3; i++) {
+		cout << endl;
+		cout << "\n NIM : " << mhs.nim;
+		cout << "\n Nama : " << mhs.nama;
+		cout << "\n Alamat : ";
+		cout << "\n \t Desa : " << mhs.alamat.desa;
+		cout << "\n \t Kota : " << mhs.alamat.kota;
+		cout << "\n Umur : " << mhs.umur;
+	}
 }
